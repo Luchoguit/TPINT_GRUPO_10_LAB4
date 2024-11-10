2286ab52561042fc973ea6d411497dadf44948af
Jose Arias <jose.arias@alumnos.frgp.utn.edu.ar>
2024-11-09 21:24:33 -0300

    Ajustes en la qry que da de alta el cliente

--- src/daoImplementacion/ClienteDaoImp.java
+++ src/daoImplementacion/ClienteDaoImp.java
@@ -14,51 +14,50 @@ import entidad.Provincia;
 
 public class ClienteDaoImp implements ClienteDao {
 
-	private static final String qryInsert = "INSERT INTO Clientes (dni, cuil, nombre, apellido, sexo, nacionalidad, fechaNacimiento, localidad, correo, telefono) "
-			+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	 private static final String qryInsert = "INSERT INTO clientes (dni, cuil, nombre, apellido, sexo, nacionalidad, fecha_nacimiento, direccion, id_localidad, id_provincia, correo, telefono) "
+	            + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-	@Override
-	public boolean altaCliente(Cliente cliente) {
-	    PreparedStatement statement;
-	    Connection conexion = Conexion.getConexion().getSQLConexion();
-	    boolean isInsertExitoso = false;
+	    @Override
+	    public boolean altaCliente(Cliente cliente) {
+	        PreparedStatement statement;
+	        Connection conexion = Conexion.getConexion().getSQLConexion();
+	        boolean isInsertExitoso = false;
 
-	    try {
-	
-	        statement = conexion.prepareStatement(qryInsert);
-
-
-	        statement.setString(1, cliente.getDni());
-	        statement.setString(2, cliente.getCuil());
-	        statement.setString(3, cliente.getNombre());
-	        statement.setString(4, cliente.getApellido());
-	        statement.setString(5, String.valueOf(cliente.getSexo()));
-	        statement.setString(6, cliente.getNacionalidad());
-	        statement.setDate(7, java.sql.Date.valueOf(cliente.getFechaNacimiento())); 
-	        statement.setString(8, cliente.getDireccion()); 
-	        statement.setInt(9, cliente.getLocalidadCliente().getId()); 
-	        statement.setInt(10, cliente.getProvinciaCliente().getId()); 
-	        statement.setString(11, cliente.getCorreo());
-	        statement.setString(12, cliente.getTelefono());
-
-
-	        if (statement.executeUpdate() > 0) {
-	            conexion.commit();
-	            isInsertExitoso = true;
-	        }
-	    } catch (SQLException e) {
-	        e.printStackTrace();
 	        try {
-	            conexion.rollback(); 
-	        } catch (SQLException e1) {
-	            e1.printStackTrace();
-	        }
-	    } finally {
-	        Conexion.getConexion().cerrarConexion(); 
-	    }
+	            statement = conexion.prepareStatement(qryInsert);
 
-	    return isInsertExitoso;
-	}
+	            // Seteando los parámetros de la consulta
+	            statement.setString(1, cliente.getDni());
+	            statement.setString(2, cliente.getCuil());
+	            statement.setString(3, cliente.getNombre());
+	            statement.setString(4, cliente.getApellido());
+	            statement.setString(5, String.valueOf(cliente.getSexo())); // 'M' o 'F'
+	            statement.setString(6, cliente.getNacionalidad());
+	            statement.setDate(7, java.sql.Date.valueOf(cliente.getFechaNacimiento()));
+	            statement.setString(8, cliente.getDireccion());
+	            statement.setInt(9, cliente.getLocalidadCliente().getId()); // id_localidad
+	            statement.setInt(10, cliente.getProvinciaCliente().getId()); // id_provincia
+	            statement.setString(11, cliente.getCorreo());
+	            statement.setString(12, cliente.getTelefono());
+
+	            // Ejecución de la consulta
+	            if (statement.executeUpdate() > 0) {
+	                conexion.commit(); // Confirmar la transacción si todo va bien
+	                isInsertExitoso = true;
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	            try {
+	                conexion.rollback(); // Revertir la transacción en caso de error
+	            } catch (SQLException e1) {
+	                e1.printStackTrace();
+	            }
+	        } finally {
+	            Conexion.getConexion().cerrarConexion(); // Cerrar la conexión
+	        }
+
+	        return isInsertExitoso;
+	    }
 
 	private static final String qrylistarclientes = "SELECT id, dni, cuil, nombre, apellido, sexo, nacionalidad, fecha_nacimiento, direccion, id_localidad, id_provincia, correo, telefono FROM clientes";
 	
